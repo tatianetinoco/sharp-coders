c8affd8e404e10977601f7be43bdd1f354a61972
tatianetinoco <tatianeperr@gmail.com>
2023-12-02 17:14:32 -0300

    TypeScript

---
+++ TypeScript/01variaveis.js
@@ -0,0 +1,27 @@
+/* 1º Conjunto de superset para JS / Linguagem.
+
+   2º Microsoft é criadora e mantedora do TS.
+
+   3º Boa parte das tecnologias front-end utilizam TS.
+
+   4º Criado pelo desenvolvedor: Anders Hejlsberg.
+*/
+
+// Variáveis
+var nome = 'Camila';
+var idade = 26;
+var estuda = true;
+var endereco = 'Centro';
+
+// Exibir Variáveis
+console.log("O nome \u00E9 ".concat(nome));
+console.log("O nome \u00E9 ".concat(idade));
+console.log("O nome \u00E9 ".concat(estuda));
+console.log("O nome \u00E9 ".concat(endereco));
+
+
+// 'Exportar' arquivo TS
+// npx tsc nome_arquivo
+
+// Executar projeto
+// node nome_arquivo.js
---
+++ TypeScript/04orientacao_objeto.tsx
@@ -0,0 +1,16 @@
+// Classe -> Molde
+
+class Pessoa{
+  // Atributos
+  nome:string;
+  idade:number;
+  cidade:string;
+}
+
+// Instanciar objeto
+const p = new Pessoa();
+p.nome = 'Bruna';
+p.idade = 22;
+p.cidade = 'São Paulo';
+
+console.log(p);
