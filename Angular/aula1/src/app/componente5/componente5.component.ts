521991cc53a0e52291f3a55971741d73fd0563aa
tatianetinoco <tatianeperr@gmail.com>
2023-12-03 01:13:15 -0300

    Angular - Aula 01

---
+++ Angular/aula1/src/app/componente5/componente5.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-componente5',
+  standalone: true,
+  imports: [],
+  templateUrl: './componente5.component.html',
+  styleUrl: './componente5.component.css'
+})
+export class Componente5Component {
+
+}
