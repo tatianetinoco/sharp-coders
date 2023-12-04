c9c5024004b00ef9fce879437617e497142f2fce
tatianetinoco <tatianeperr@gmail.com>
2023-12-04 20:32:08 -0300

    Angular - Aula 01 (continuação)

--- Angular/aula1/src/app/componente3/componente3.component.ts
+++ Angular/aula1/src/app/componente3/componente3.component.ts
@@ -1,12 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-componente3',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './componente3.component.html',
   styleUrl: './componente3.component.css'
 })
 export class Componente3Component {
 
+  // Variável de média
+  media: number = 8;
+
+  // Variável de linguagem
+  linguagem: string = 'CSS'
+
+  // Vetor de nomes
+  nomes:string[] = ['Luciana', 'Renato', 'Carla'];
 }
--- Angular/aula1/src/app/componente4/componente4.component.ts
+++ Angular/aula1/src/app/componente4/componente4.component.ts
@@ -1,12 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-componente4',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './componente4.component.html',
   styleUrl: './componente4.component.css'
 })
 export class Componente4Component {
+
+  // Variável para armazenar texto
+
+  texto:string = '';
+
+  // Variáveis para armazenar os inputs do formulário
+  email:string = '';
+  senha:string = ''
 
 }
