b38bd6ed5fa126f6c0953cacb23cddcadfa40d2a
tatianetinoco <tatianeperr@gmail.com>
2023-12-22 22:14:22 -0300

    Angular - Aula 07

---
+++ Angular/aula7/src/app/Pessoa.ts
@@ -0,0 +1,12 @@
+export class Pessoa{
+
+    // Atributos
+    private nome?:string;
+    private idade?:number;
+
+    // Construtor
+    constructor(nome:string, idade:number){
+        this.nome = nome;
+        this.idade = idade;
+    }
+}
