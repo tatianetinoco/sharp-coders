0cbea445226abc47ccf40330585ebf518934aae0
tatianetinoco <tatianeperr@gmail.com>
2024-01-22 01:29:49 -0300

    C# Aula 06 - 19/01

---
+++ csharp/samples/Nullables/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Nullables;
+
+public class Program
+{
+  public static void Main(string[] args)
+  {
+      double valor = double.Parse(Console.ReadLine()!);
+
+      int? nota = null;
+
+      Console.WriteLine(nota);
+
+      double? salarioTrabalhoVoluntario = null;
+      double? salarioSemTrabalho = 0.0;
+
+      // trabalho voluntário = recebe 0 reais
+      Console.WriteLine(salarioTrabalhoVoluntario ?? 0.0);
+
+      // salário sem trabalho = recebe 0 reais
+      Console.WriteLine(salarioSemTrabalho ?? 0.0);
+  }
+
+}
+
---
+++ csharp/samples/RefInOutKeywords/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace RefInOutKeywords;
+
+public class Program {
+
+  public static void Troca(ref int a, ref int b) {
+    int aux = a;
+    a = b;
+    b = aux;
+  }
+
+  public static void Imprime(in double valor) {
+    // consegue garantir imutabilidade da variável
+    // valor = 200.0; // não funciona
+    Console.WriteLine($"Valor: R${valor:F2}");
+  }
+
+  public static void Inicializa(out int pontuacao) {
+    pontuacao = 100;
+  }
+
+  public static void Main(string[] args)
+  {
+    int a = 3, b = 2;
+    Console.WriteLine($"A: {a}, B: {b}"); // A: 3, B: 2
+
+    Troca(ref a, ref b);
+
+    Console.WriteLine($"A: {a}, B: {b}"); // A: 2, B: 3
+
+    double valor = 239.99;
+    Imprime(in valor);
+
+    // int pontuacao;
+    Inicializa(out int pontuacao);
+    Console.WriteLine(pontuacao);
+    
+  }
+}
+
